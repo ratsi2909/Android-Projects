3a35c799e08dfcae61b7d9bf1d97ce803c810b11
ratsi2909 <rathi2909@gmail.com>
2021-12-07 22:58:19 -0600

    changes to send text

--- app/src/main/java/edu/utexas/mpc/StepcountsApp/MainActivity.kt
+++ app/src/main/java/edu/utexas/mpc/StepcountsApp/MainActivity.kt
@@ -1,20 +1,19 @@
 package edu.utexas.mpc.StepcountsApp
 
-import android.content.DialogInterface
-import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.provider.Telephony
 import android.support.annotation.RequiresApi
+import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.telephony.SmsManager
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -28,6 +27,11 @@ import java.time.LocalDate
 import java.time.LocalDate.now
 import java.time.LocalDateTime
 import java.util.*
+import android.Manifest
+import android.app.Activity
+import android.app.Dialog
+import android.content.*
+import android.text.InputType
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,15 +40,20 @@ class MainActivity : AppCompatActivity() {
     // I'm using lateinit for these widgets because I read that repeated calls to findViewById
     // are energy intensive
     lateinit var textView: TextView
+    lateinit var phonenum: EditText
+    lateinit var smstext: EditText
+    lateinit var imageView: ImageView
     lateinit var retrieveButton: Button
     lateinit var switchwifiButton: Button
     lateinit var goalcheckinButton: Button
+    lateinit var smsButton: Button
 
     lateinit var queue: RequestQueue
     lateinit var gson: Gson
     lateinit var mostRecentWeatherResult: WeatherResult
     lateinit var mostRecentWeatherForecast: ForeCastWeatherResult
     lateinit var mqttAndroidClient: MqttAndroidClient
+    lateinit var todaysDate: String
 
     // you may need to change this depending on where your MQTT broker is running
     val serverUri = "tcp://192.168.4.14:1883"
@@ -57,8 +66,8 @@ class MainActivity : AppCompatActivity() {
 
     var weatherData  = ""
     var weatherForecast  = ""
+    var editTextInput = ""
 
-    lateinit var todaysDate: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,14 +77,30 @@ class MainActivity : AppCompatActivity() {
         retrieveButton = this.findViewById(R.id.retrieveButton)
         switchwifiButton = this.findViewById(R.id.switchwifiButton)
         goalcheckinButton = this.findViewById(R.id.goalcheckinButton)
+        smsButton = this.findViewById(R.id.smsButton)
+        phonenum = findViewById<EditText>(R.id.phonenum)
+        smstext = findViewById<EditText>(R.id.smstext)
 
-        // keep goal /switch wifi button invisible on create
+        // keep goal /switch wifi/sms button invisible on create
         switchwifiButton.setVisibility(View.INVISIBLE)
         goalcheckinButton.setVisibility(View.INVISIBLE)
+        smsButton.setVisibility(View.INVISIBLE)
+        phonenum.setVisibility(View.INVISIBLE)
+        smstext.setVisibility(View.INVISIBLE)
 
-        // button actions
+        // check permissions for sending text
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.RECEIVE_SMS,Manifest.permission.SEND_SMS),111)
+        }
+        //else
+        //    receiveMsg()
+
+        // text button actions
+        smsButton.setOnClickListener({ sendsms() })
+
+        // other button actions
         switchwifiButton.setOnClickListener({ switchwifi() })
-
         goalcheckinButton.setOnClickListener({ syncWithPi() })
         retrieveButton.setOnClickListener({ requestWeather() })
 
@@ -108,6 +133,10 @@ class MainActivity : AppCompatActivity() {
             override fun messageArrived(topic: String?, message: MqttMessage?) {
                 println(message)
                 textView.text = message.toString()
+                retrieveButton.setVisibility(View.VISIBLE)
+                if(textView.text.contains("Goal"))
+                    smsdialog()
+
             }
 
             override fun connectionLost(cause: Throwable?) {
@@ -121,9 +150,22 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    // validating permission
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode==111 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            receiveMsg()
+    }
+
     // this method is used to get current day's weather
     fun requestWeather(){
         //Sending stuff to PI
+        println("requesting weather")
+        retrieveButton.setVisibility(View.INVISIBLE)
         val url = StringBuilder("https://api.openweathermap.org/data/2.5/weather?id=4254010&appid=ea9d4a9da52aaa9920a1f1ab27e0086e").toString()
         val stringRequest = object : StringRequest(com.android.volley.Request.Method.GET, url,
             com.android.volley.Response.Listener<String> { response ->
@@ -135,10 +177,10 @@ class MainActivity : AppCompatActivity() {
 
                 val iconUrl = "https://openweathermap.org/img/w/$icon.png"
                 //print icon in image view
-                val imageView: ImageView = this.findViewById(R.id.image_view)
+                imageView = this.findViewById(R.id.image_view)
                 Picasso.with(this).load(iconUrl).into(imageView)
 
-                Log.v("MainActivity", url)
+                Log.v("request weather", url)
 
                 val sdf = java.text.SimpleDateFormat("yyyy-MM-dd")
                 val date = java.util.Date(mostRecentWeatherResult.dt *1000)
@@ -156,16 +198,12 @@ class MainActivity : AppCompatActivity() {
                 switchwifiButton.setVisibility(View.VISIBLE)
                 goalcheckinButton.setVisibility(View.INVISIBLE)
 
-                //val toast = Toast.makeText(this, "Click Switch wifi and follow steps to do Goal Check-in", Toast.LENGTH_LONG)
-                //toast.setGravity(Gravity.CENTER, 0, 0)
-
-                //toast.show()
                 val toast = Toast.makeText(this, "Click Switch wifi and follow steps to do Goal Check-in", Toast.LENGTH_LONG)
                 val textView = toast.view.findViewById(android.R.id.message) as TextView
                 textView.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
                 toast.show()
             },
-            com.android.volley.Response.ErrorListener { println("******That didn't work!") }) {}
+            com.android.volley.Response.ErrorListener { println("******request weather didn't work!") }) {}
         // Add the request to the RequestQueue.
         queue.add(stringRequest)
     }
@@ -212,12 +250,15 @@ class MainActivity : AppCompatActivity() {
         queue.add(forecastRequest)
     }
 
+
     // this method is used to alert user to switch wifi
     fun switchwifi(){
         println("+++++++ allowing user to switch wifi ...")
         var builder = AlertDialog.Builder(this )
+
         builder.setTitle(getString(R.string.switch_wifi))
         builder.setMessage(getString(R.string.switch_wifi_message))
+
 
         builder.setPositiveButton("ok", DialogInterface.OnClickListener{ dialog, id ->
             startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))
@@ -225,6 +266,7 @@ class MainActivity : AppCompatActivity() {
         })
         builder.setOnDismissListener(DialogInterface.OnDismissListener() {
             adjustVisibility()
+
         })
         var alert = builder.create()
         alert.show()
@@ -242,7 +284,11 @@ class MainActivity : AppCompatActivity() {
         builder.setPositiveButton("yes", DialogInterface.OnClickListener{ dialog, id ->
             switchwifiButton.setVisibility(View.INVISIBLE)
             goalcheckinButton.setVisibility(View.VISIBLE)
+//            smsButton.setVisibility(View.VISIBLE)
+//            phonenum.setVisibility(View.VISIBLE)
+//            smstext.setVisibility(View.VISIBLE)
             dialog.cancel()
+        //    requestWeather()
         })
         builder.setNegativeButton(getString(R.string.no), DialogInterface.OnClickListener{dialog,id ->
             dialog.cancel()
@@ -251,11 +297,83 @@ class MainActivity : AppCompatActivity() {
         alert.show()
     }
 
+    // this method is used to alert user to switch wifi
+    fun smsdialog(){
+        println("+++++++ allowing user to send text ...")
+        var builder = AlertDialog.Builder(this )
+
+        builder.setTitle("Send Text to friend")
+        builder.setMessage("Would you like to share your success to your friend?")
+
+        builder.setPositiveButton("yes", DialogInterface.OnClickListener{ dialog, id ->
+            sendtext((this@MainActivity))
+            dialog.dismiss()
+        })
+        builder.setNegativeButton(getString(R.string.no), DialogInterface.OnClickListener{dialog,id ->
+            dialog.cancel()
+        })
+        var alert = builder.create()
+        alert.show()
+
+    }
+
+    fun sendtext(activity: Activity){
+        println("+++++++ allowing user to send text ...")
+        val dialog = Dialog(activity)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.dialog_layout)
+
+        var et2: EditText? = null
+        var et3: EditText? = null
+
+        et2 = dialog.findViewById(R.id.et2)
+        et3 = dialog.findViewById(R.id.et3)
+
+        val btnok = dialog.findViewById(R.id.btnok) as Button
+        btnok.setOnClickListener {
+            var sms = SmsManager.getDefault()
+            sms.sendTextMessage(et2.getText().toString(),"Group4",et3.getText().toString(),null,null)
+            dialog.dismiss()
+        }
+
+        dialog.show()
+
+    }
+
     // this method is used to set up MQTT connection
     fun syncWithPi(){
         println("+++++++ Setting MQTT connection ...")
         mqttAndroidClient.connect()
+        //switchwifi()
+        //retrieveButton.setVisibility(View.VISIBLE)
+        //textView.setText("")
+        //imageView.setImageBitmap(null)
     }
+
+    fun sendsms(){
+        println("+++++++ send sms ...")
+        println(phonenum)
+        var sms = SmsManager.getDefault()
+        sms.sendTextMessage(phonenum.text.toString(),"Group4",smstext.text.toString(),null,null)
+    }
+
+    private fun receiveMsg(){
+        println("+++++++ receive sms ...")
+        var br = object: BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+                    for(sms in Telephony.Sms.Intents.getMessagesFromIntent(p1)){
+                        Toast.makeText(applicationContext,sms.displayMessageBody,Toast.LENGTH_LONG).show()
+                        phonenum.setText(sms.originatingAddress)
+                        smstext.setText(sms.displayMessageBody)
+                    }
+                }
+            }
+        }
+        registerReceiver(br, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
+    }
+
+
 }
 
 class WeatherResult(val id: Int, val name: String, val cod: Int, val coord: Coordinates, val dt: Long,val main: WeatherMain,val weather: Array<Weather>  )
